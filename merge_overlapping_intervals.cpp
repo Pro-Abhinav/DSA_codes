d4db16b462d714d5e3913117158488f8c008df55
Abhinav <abhinav16032002@gmail.com>
2020-11-16 20:49:18 +0530

    Merge overlapping intervals

--- merge_overlapping_intervals.cpp
+++ merge_overlapping_intervals.cpp
@@ -16,26 +16,33 @@ int mergeIntervals(int *ar, int siz)
 
     for(int i=1;i<siz-1;i+=2)
     {
-        cout<<ar[i+1];
         if(_end > ar[i+1])
         {
             _end = ar[i+2];
-            for(int j=i;j<siz;j++)
-            {
-                ar[j]=ar[j+2];
-            }
-            siz-=2;
+            ar[i+1]=INT_MAX;
+            ar[i] = INT_MAX;
         }
-        cout<<ar[i+1];
-        cout<<ar[i]<<_end<<"\n";
+    }
+
+    for(int i=0;i<siz;i++)
+    {
+        if(ar[i]==INT_MAX)
+        {
+            int j;
+            for(j=i;j<siz;j++)
+            {
+                ar[j]=ar[j+1];
+            }
+            siz--;
+            i--;
+        }
     }
     printArray(ar, siz);
-    cout<<"\n"<<siz;
 }
 
 int main()
 {
-    int ar[] = {1,3,2,4,3,6,7,8};
+    int ar[] = {1,3,2,4,3,6,7,8,8,9};
     int siz = sizeof(ar)/sizeof(ar[0]);
 
     mergeIntervals(ar, siz);
