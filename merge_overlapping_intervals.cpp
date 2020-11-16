fc928a301c3718635166d800e55f2ec192000cce
Abhinav <abhinav16032002@gmail.com>
2020-11-16 21:39:41 +0530

    Merge overlapping intervals

--- merge_overlapping_intervals.cpp
+++ merge_overlapping_intervals.cpp
@@ -1,49 +1,48 @@
-#include<iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void printArray(int *ar, int siz)
+struct Interval{
+    int s, e;
+};
+
+bool compareInterval(Interval a, Interval b)
 {
-    for(int i=0;i<siz;i++)
-    {
-        cout<<ar[i];
-    }
+    return a.s < b.s;
 }
 
-int mergeIntervals(int *ar, int siz)
+void mergeInterval(Interval *ar, int siz)
 {
-    int _start = ar[0], _end =ar[1];
+    sort(ar, ar+siz, compareInterval);
 
-    for(int i=1;i<siz-1;i+=2)
+    int index = 0;
+
+    for(int i=1; i<siz; i++)
     {
-        if(_end > ar[i+1])
+        if(ar[index].e >= ar[i].s)
         {
-            _end = ar[i+2];
-            ar[i+1]=INT_MAX;
-            ar[i] = INT_MAX;
+            ar[index].e = max(ar[index].e, ar[i].e);
+            ar[index].s = max(ar[index].s, ar[i].s);
+        }
+        else{
+            index++;
+            ar[index] = ar[i];
         }
     }
 
-    for(int i=0;i<siz;i++)
+    cout<<"\nFinal merged arrays are: ";
+    for(int i=0; i<=index; i++)
     {
-        if(ar[i]==INT_MAX)
-        {
-            int j;
-            for(j=i;j<siz;j++)
-            {
-                ar[j]=ar[j+1];
-            }
-            siz--;
-            i--;
-        }
+        cout<< "["<<ar[i].s<<","<<ar[i].e<<"]";
     }
-    printArray(ar, siz);
 }
 
 int main()
 {
-    int ar[] = {1,3,2,4,3,6,7,8,8,9};
+    Interval ar[] = { {6,8}, {1,9}, {2,4}, {4,7} };
     int siz = sizeof(ar)/sizeof(ar[0]);
 
-    mergeIntervals(ar, siz);
+    mergeInterval(ar, siz);
+
+    return 0;
 }
